22813f2399160dbe2864f1208a335e2f4657fb66
Priidu Paomets <priidu.paomets@gmail.com>
2019-12-08 19:19:16 +0200

    Added API-Cache module
    Added caching using default cache

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Module requires
 var express = require('express');
 var logger = require('morgan');
+var apicache = require('apicache');
 var routes = require('./routes');
 
 // Instatiate application instance
 var app = express();
+
+// instantiate default API cache
+let cache = apicache.middleware;
 
 // Configure logger middleware
 app.use(logger('dev'));
@@ -21,15 +25,15 @@ app.get('/api', routes.apiIndex);
 // Application-specific routes
 // app.get('/api/users/:id([0-9]{1,9})?', routes.usersByID);
 // app.get('/api/users/:username?', routes.usersByUsername);
-app.get('/api/users/:id?', routes.users);
+app.get('/api/users/:id?', cache('10 seconds'), routes.users);
 
-app.get('/api/frontpage', routes.frontpage);
-app.get('/api/profile/:id', routes.profilePage);
-app.get('/api/posts/:id', routes.postDetails);
-app.get('/api/stats', routes.statistics);
-app.get('/api/stats/top10/postingusers', routes.top10PostingUsers);
-app.get('/api/stats/registrations', routes.userRegistrations);
-app.get('/api/stats/genderdivision', routes.genderDivision);
+app.get('/api/frontpage', cache('10 seconds'), routes.frontpage);
+app.get('/api/profile/:id', cache('10 seconds'), routes.profilePage);
+app.get('/api/posts/:id', cache('10 seconds'), routes.postDetails);
+app.get('/api/stats', cache('10 seconds'), routes.statistics);
+app.get('/api/stats/top10/postingusers', cache('10 seconds'), routes.top10PostingUsers);
+app.get('/api/stats/registrations', cache('10 seconds'), routes.userRegistrations);
+app.get('/api/stats/genderdivision', cache('10 seconds'), routes.genderDivision);
 
 // Default response when no matching routes were found
 app.get('*', routes.default);
